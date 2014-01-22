53f58d208bf8156c9290f7d5c9d0335468fc2e72
kaelzhang <i@kael.me>
2014-01-23 01:31:53 +0800

    add pages

---
+++ page/order.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var $ = require('jquery');
+var Ajax = require('request').Ajax;
+var tpl = require('tpl');
+
+var template = require('../view/order');
+var templateFn = tpl.compile(template);
+
+var order = exports;
+
+order.render = function (query) {
+    function start () {
+        order._get_data(query.usertoken, function (json) {
+            order._apply_data(json);
+        })
+    }
+
+    setInterval(start, 3000);
+    start();
+};
+
+
+order._apply_data = function (json) {
+    $('#container').html(templateFn(json));
+};
+
+
+order._get_data = function (token, callback) {
+    new Ajax({
+        url: '/tables?token=' + token,
+        method: 'get'
+
+    }).on('success', callback).send();
+};
+
+
