e266971634bf5a5924f0f7d9f03c0eba7b00ce0f
kaelzhang <i@kael.me>
2014-01-27 11:24:33 +0800

    complete server

--- server/app.js
+++ server/app.js
@@ -15,51 +15,150 @@ function random (max, min) {
     return parseInt( Math.random() * (max - min) + min );
 }
 
-app.get('/:root/:action', function (req, res, next) {
+
+var users = {};
+var tables = {
+    a: {
+        // 
+        current: 0,
+        max: 0
+    },
+
+    b: {
+        current: 0,
+        max: 0
+    },
+
+    c: {
+        current: 0,
+        max: 0
+    }
+};
+
+
+function getTables () {
+    return Object.keys(tables).map(function (name) {
+        var table = tables[name];
+
+        return {
+            name: name,
+            num: table.current,
+            time: random(30)
+        };
+    });
+}
+
+
+function getUser (usertoken) {
+    var user = users[usertoken];
+
+    if ( !user ) {
+        return null;
+    }
+
+    var tableName = user.table;
+    var table = tables[tableName];
+
+    return {
+        table: tableName,
+        num: user.num,
+        left: user.num - table.current
+    };
+}
+
+
+function takeTable (usertoken, tableName) {
+    var user = getUser(usertoken);
+    var table = tables[tableName];
+
+    if ( !table ) {
+        return {
+            error: 'table "' + tableName + '" not found'
+        };
+    }
+
+    if ( !user ) {
+        users[usertoken] = {
+            table: tableName,
+            num: ++ table.max
+        };
+
+        return {
+            ok: true
+        }
+
+    } else {
+        if ( user.table === tableName ) {
+            return {
+                error: 'user "' + usertoken + '" already has a table'
+            }
+        } else {
+            return {
+                error: 'Your can not order more than one table'
+            }
+        }
+    }
+}
+
+
+function manageTable (tableName) {
+    var table = tables[tableName];
+
+    if ( table ) {
+        if ( table.max > table.current ) {
+            table.current ++;
+            return {
+                ok: true
+            }
+        } else {
+            return {
+                error: 'there is no more tables'
+            }
+        }
+
+    } else {
+        return {
+            error: 'table "' + tableName + '" not found'
+
+        }
+    }
+}
+
+
+app.get('/:action', function (req, res, next) {
     var obj;
+
+    var usertoken = req.query.usertoken;
+    var tableName = req.query.table;
 
     switch(req.params.action){
         case 'tables.jsp':
+
             obj = {
-                shop: {
-                    name: '屠屠餐厅'
-                },
-
-                tables: [
-                    {
-                        name: 'A',
-                        num: random(10),
-                        time: 10 
-                    },
-
-                    {
-                        name: 'B',
-                        num: random(10),
-                        time: 4
-                    }
-                ]
+                tables: getTables()
             };
 
-            if ( random(10) > 5 ) {
-                obj.my = {
-                    table: 'A',
-                    num: random(10),
-                    left: 3
-                };
-            }
+            if ( usertoken ) {
+                var user = getUser(usertoken);
+
+                if ( user ) {
+                    obj.my = user;
+                }
+            }            
 
             break;
 
         case 'take.jsp':
-            obj = {
+            if ( usertoken && tableName ) {
+                obj = takeTable(usertoken, tableName);
+            }
 
-            };
             break;
 
         case 'admin.jsp':
-            obj = {
-
-            };
+            if ( tableName ) {
+                obj = manageTable(tableName);
+            }
 
             break;
     }
@@ -76,3 +175,4 @@ app.get('/:root/:action', function (req, res, next) {
 app.listen(9987, function () {
     console.log('server started at localhost:9987');
 });
+
